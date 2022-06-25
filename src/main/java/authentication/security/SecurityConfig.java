b0777909119e7216adbd6a0466fa64601fa5c8b9
sammuelfer <samuelfesant@gmail.com>
2022-06-25 11:35:35 -0300

    Configure endpoints abertos

---
+++ src/main/java/authentication/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package authentication.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/users/create", "/users/create/**")
+                .permitAll().and().httpBasic();
+    }
+}
