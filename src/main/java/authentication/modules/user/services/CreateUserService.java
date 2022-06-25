d4e846614e1fcaffa2250bf1617b084fe3de2772
sammuelfer <samuelfesant@gmail.com>
2022-06-25 11:37:38 -0300

    Encriptografa a senha do usuário

--- src/main/java/authentication/modules/user/services/CreateUserService.java
+++ src/main/java/authentication/modules/user/services/CreateUserService.java
@@ -3,6 +3,7 @@ package authentication.modules.user.services;
 import authentication.modules.user.entities.User;
 import authentication.modules.user.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,10 @@ public class CreateUserService {
   @Autowired
   UserRepository userRepository;
 
+  private BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
   public User create(User user) {
 
     User existsUser = userRepository.findByUsername(user.getUsername());
@@ -18,6 +23,8 @@ public class CreateUserService {
     if (existsUser != null) {
       throw new Error("User already exists!");
     }
+
+    user.setPassword(passwordEncoder().encode(user.getPassword()));
 
     User createdUser = userRepository.save(user);
 
