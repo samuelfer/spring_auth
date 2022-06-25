45cd8d1ce6ba946a534c44b7368644ed2e4f170c
sammuelfer <samuelfesant@gmail.com>
2022-06-25 14:08:41 -0300

    Ajuste

--- src/main/java/authentication/security/SecurityConfig.java
+++ src/main/java/authentication/security/SecurityConfig.java
@@ -27,8 +27,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests().antMatchers("/users/create", "/users/create/**")
-                .permitAll().and().httpBasic();
+        http.authorizeRequests().antMatchers("/users/create", "/users/create/**")
+                .permitAll().and().httpBasic()
+                .and()
+                .csrf().disable();
     }
 }
